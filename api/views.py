8cb48d2bcd7abc106bf62d90f43c780a0910ccaf
JyunKim <kann1997@naver.com>
2020-10-01 18:33:39 +0900

    Add serializers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from rest_framework.views import APIView
+from .serializers import LectureSerializer, ProfessorSerializer, RankSerializer, ResultSerializer, ProfileSerializer
 
-# Create your views here.
+
+class LectureList(APIView):
+    def get(self, request):
+
+    def post(self, request):
