f20746625c0d7187a6bebe2e31c27b4a3a039913
JyunKim <kann1997@naver.com>
2020-12-27 02:04:38 +0900

    Modify admin

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Lecture, Profile, Rank, Result
+from .models import Lecture, Profile, Rank, Result, Professor
 
 
 admin.site.register(Lecture)
 admin.site.register(Profile)
 admin.site.register(Rank)
 admin.site.register(Result)
+admin.site.register(Professor)
